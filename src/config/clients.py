98fe08490d972b3d4c4dc6c7c6eb31395e05614a
aaron6260 <57122978+aaron6260@users.noreply.github.com>
2026-07-18 15:40:34 -0600

    Added business size and business scale factor to the client dataframe.

--- src/config/clients.py
+++ src/config/clients.py
@@ -1,17 +1,36 @@
 # Configuration file for clients.
 
 INDUSTRY_CONFIG = {
-    "Technology": 10,
-    "Healthcare": 10,
-    "Utilities": 15,
-    "Retail": 3,
-    "Manufacturing": 20,
-    "Agriculture": 1,
-    "Education": 5,
-    "Transportation": 5,
-    "Energy": 30,
-    "Telecommunications": 1
+    "Technology": {"weight": 10},
+    "Healthcare": {"weight": 10},
+    "Utilities": {"weight": 15},
+    "Retail": {"weight": 3},
+    "Manufacturing": {"weight": 20},
+    "Agriculture": {"weight": 1},
+    "Education": {"weight": 5},
+    "Transportation": {"weight": 5},
+    "Energy": {"weight": 30},
+    "Telecommunications": {"weight": 1}
 }
 
 CLIENT_PREFIXES = ['Blue', 'Alpha', 'Legacy', 'NextGen', 'Pinnacle', 'Summit', 'Vertex', 'Horizon', 'Apex', 'Zenith']
 CLIENT_SUFFIXES = ['Solutions', 'Systems', 'Technologies', 'Enterprises', 'Industries', 'Dynamics', 'Innovations', 'Global', 'Networks', 'Partners']
+
+CLIENT_SIZE_CONFIG = {
+    "Small": {
+        "business_scale_range": (0.5, 0.9),
+        "probability": .45
+    },
+    "Medium": {
+        "business_scale_range": (1.0, 1.8),
+        "probability": .30
+    },
+    "Large": {
+        "business_scale_range": (2.0, 3.2),
+        "probability": .18
+    },
+    "Enterprise": {
+        "business_scale_range": (3.5, 5.0),
+        "probability": 0.07
+    }
+}
--- src/generate_data.py
+++ src/generate_data.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 
 from config.general import NUM_CLIENTS, NUM_PROJECTS, START_YEAR, END_YEAR, RANDOM_SEED
-from config.clients import INDUSTRY_CONFIG, CLIENT_PREFIXES, CLIENT_SUFFIXES
+from config.clients import INDUSTRY_CONFIG, CLIENT_PREFIXES, CLIENT_SUFFIXES, CLIENT_SIZE_CONFIG
 from config.projects import PROJECT_CATALOG, REGIONS, PROJECT_MANAGERS, STATUS_DISTRIBUTION
 
 ## Setting deterministic randomness for reproducability
@@ -39,14 +39,20 @@ def generate_clients(num_clients=NUM_CLIENTS):
     data = {
         'client_id': range(1, num_clients + 1),
         'client_name': client_names,
-        #'industry': random.choices(INDUSTRIES, weights=INDUSTRY_WEIGHTS, k=num_clients)
-        'industry': random.choices(list(INDUSTRY_CONFIG.keys()), weights=list(INDUSTRY_CONFIG.values()), k=num_clients)
+        'industry': random.choices(list(INDUSTRY_CONFIG.keys()), weights=[v['weight'] for v in INDUSTRY_CONFIG.values()], k=num_clients),
+        'client_size': random.choices(list(CLIENT_SIZE_CONFIG.keys()), weights=[v['probability'] for v in CLIENT_SIZE_CONFIG.values()], k=num_clients),
     }
     clients_df = pd.DataFrame(data)
+    clients_df['business_scale'] = clients_df.apply(lambda row: random.uniform(*CLIENT_SIZE_CONFIG[row['client_size']]['business_scale_range']), axis=1)
     return clients_df
 
 def generate_projects(num_projects=NUM_PROJECTS, clients_df=None):
     projects_df = None
+    data = {
+        'project_id': range(1, num_projects + 1),
+        'client_id': random.choices(clients_df['client_id'], k=num_projects),
+        
+    }
     return projects_df
 
 def generate_monthly_costs():
@@ -63,7 +69,7 @@ def save_csvs():
 
 def main():
     clients = generate_clients(num_clients=NUM_CLIENTS)
-    print(clients)
+    #print(clients)
 #    generate_projects()
 #    generate_monthly_costs()
 #    generate_change_orders()
